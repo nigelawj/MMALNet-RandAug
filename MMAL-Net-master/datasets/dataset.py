fb6887a56865de7d32c2e8f0386a44682d2c7437
Nigel <nigelawj@gmail.com>
2020-09-22 17:06:25 +0800

    fixed issue with path

--- MMAL-Net-master/datasets/dataset.py
+++ MMAL-Net-master/datasets/dataset.py
@@ -6,6 +6,7 @@ from torchvision import transforms
 import torch
 
 import pandas as pd
+from pathlib import Path
 
 class CUB():
     def __init__(self, input_size, root, is_train=True, data_len=None):
@@ -214,9 +215,9 @@ class CompCars():
         test_img_label = []
 
         for i in train_label_df.index:
-            train_img_label.append([os.path.join(train_img_path, train_label_df['img_path'][i]), int(train_label_df['make_code'][i])]) # make_code [0, 74]
+            train_img_label.append([Path(train_img_path, train_label_df['img_path'][i]), int(train_label_df['make_code'][i])]) # make_code [0, 74]
         for i in test_label_df.index:
-            test_img_label.append([os.path.join(train_img_path, train_label_df['img_path'][i]), int(train_label_df['make_code'][i])])
+            test_img_label.append([Path(train_img_path, train_label_df['img_path'][i]), int(train_label_df['make_code'][i])])
 
         self.train_img_label = train_img_label[:data_len]
         self.test_img_label = test_img_label[:data_len]
